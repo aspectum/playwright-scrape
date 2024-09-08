ecf3e1c5b6d6e295fbe261f3424edfc73082c573
Pedro Garcia <drogpe@gmail.com>
2024-09-08 20:25:55 -0300

    feat: add make-ebook script

---
+++ src/make-ebook.sh
@@ -0,0 +1,1 @@
+pandoc -f html -t epub --epub-metadata output/metadata.xml --toc --epub-cover-image=output/cover.jpg --resource-path=output --css=src/book.css -o output/wcss.epub output/filtered.html
